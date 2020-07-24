89a417ebfb85e8d365a0b3f60267cdd16a9ce6d0
paulovictor11 <pauloalmeidajp652@gmail.com>
2020-07-24 12:39:02 -0300

    Atualizando repositorio

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <product-list-one></product-list-one>
+        <product-list-two></product-list-two>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import ProductListOne from './components/ProductListOne';
+    import ProductListTwo from './components/ProductListTwo';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        data () {
+            return {
+                
+            }
+        },
+        components: {
+            ProductListOne, ProductListTwo
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
